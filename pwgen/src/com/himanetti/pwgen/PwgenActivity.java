0919ef9dc0daa89a04ced7acad6305b2afddc3cb
Henri Björkman <henri.bjorkman@gulassi.net>
2011-12-24 17:14:44 +0200

    First commit

---
+++ pwgen/src/com/himanetti/pwgen/PwgenActivity.java
@@ -0,0 +1,13 @@
+package com.himanetti.pwgen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PwgenActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
