c8d48ac188ed518ee84a5ce3356cf02c8bb1c0e4
Henri Björkman <henri.bjorkman@gulassi.net>
2012-01-15 12:41:38 +0200

    Changed from char[] to Character[].
    Implemented removal of ambiguous characters from password.

--- pwgen/src/com/himanetti/pwgen/PwgenActivity.java
+++ pwgen/src/com/himanetti/pwgen/PwgenActivity.java
@@ -7,9 +7,13 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.LinkedList;
 import java.util.Random;
+import java.util.Arrays;
 
 public class PwgenActivity extends Activity {
 	
@@ -17,37 +21,47 @@ public class PwgenActivity extends Activity {
 	private Button mGeneratePasswd;
 	private EditText mPasswdLength;
 	private EditText mGeneratedPasswd;
+	private CheckBox mAmbiquous;
 	private String generatedPasswd;
 	public Integer passwdType;
 	
 	// Set different password character types
-	private char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
-	private char[] lowercase = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+	private Character[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	private Character[] lowercase = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
 								'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
 								'w', 'x', 'y', 'z' };
-	private char[] uppercase = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+	private Character[] uppercase = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
 								'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
 								'W', 'X', 'Y', 'Z' };
-	private char[] pwCharacters;
+	private Character[] ambiquous = { '0', 'O', 'l', '1' };
+	private Character[] pwCharacters;
 	
 	// Create random generator
 	static Random generator = new Random();
 	
-	public static char[] arrayMerge(char[]... arrays) {
+	public static Character[] removeAmbiquous(Character[] a, Character[] b) {
+		
+		LinkedList<Character> characters = new LinkedList<Character>(Arrays.asList(a));
+		characters.removeAll(Arrays.asList(b));
+		Character[] contents = new Character[characters.size()];
+		return (Character[]) characters.toArray(contents);
+	}
+	
+	public static Character[] arrayMerge(Character[]... arrays) {
 		
 		// Determine required size of new array
 	    int count = 0;
-	    for (char[] array : arrays)
+	    for (Character[] array : arrays)
 	    {
 	        count += array.length;
 	    }
 		
 	    // create new array of required class
-	    char[] mergedArray = new char[count];
+	    Character[] mergedArray = new Character[count];
 
 	    // Merge each array into new array
 	    int start = 0;
-	    for (char[] array : arrays) {
+	    for (Character[] array : arrays) {
 	        System.arraycopy(array, 0, mergedArray, start, array.length);
 	        start += array.length;
 	    }
@@ -72,6 +86,7 @@ public class PwgenActivity extends Activity {
         mGeneratePasswd = (Button) findViewById(R.id.button1);
         mPasswdLength = (EditText) findViewById(R.id.editText1);
         mGeneratedPasswd = (EditText) findViewById(R.id.editText2);
+        mAmbiquous = (CheckBox) findViewById(R.id.checkBox1);
          
         // Set what to do on UI actions
         mGeneratePasswd.setOnClickListener(new View.OnClickListener() {
@@ -87,7 +102,6 @@ public class PwgenActivity extends Activity {
     protected String generatePasswd(Integer passwdType, Integer passwdLength) {
     	
     	StringBuffer output = new StringBuffer(passwdLength);
-    	Integer i = 0;
 		switch (passwdType) {
     		case 0:
     			pwCharacters = lowercase;	
@@ -108,9 +122,12 @@ public class PwgenActivity extends Activity {
     			pwCharacters = arrayMerge(lowercase, uppercase, numbers);
     			break;
     	}
-		while (i < passwdLength) {
+		if (mAmbiquous.isChecked()) {
+			pwCharacters = removeAmbiquous(pwCharacters, ambiquous);
+		}
+		
+		while (output.length() < passwdLength) {
 			output.append(selectRandom.get(pwCharacters));
-			i++;
 		};
 
 		return output.toString();
@@ -131,7 +148,7 @@ public class PwgenActivity extends Activity {
 }    
 
 class selectRandom {
-	public static char get (char[] pwCharacters) {
+	public static Character get (Character[] pwCharacters) {
         int rnd = PwgenActivity.generator.nextInt(pwCharacters.length);
         return pwCharacters[rnd];
     }
